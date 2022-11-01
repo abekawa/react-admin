a94f09f09c5b428b45ddb7b0ec2268c523f2aeb7
André Abekawa <andre.abekawa@gmail.com>
2022-11-01 08:50:20 -0300

    Init repo

---
+++ src/views/user/login.js
@@ -0,0 +1,140 @@
+import React, { useState, useEffect } from 'react';
+import { Row, Card, CardTitle, Label, FormGroup, Button } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { Formik, Form, Field } from 'formik';
+import { NotificationManager } from 'components/common/react-notifications';
+
+import { loginUser } from 'redux/actions';
+import { Colxx } from 'components/common/CustomBootstrap';
+import IntlMessages from 'helpers/IntlMessages';
+
+const validatePassword = (value) => {
+  let error;
+  if (!value) {
+    error = 'Digite aqui sua senha';
+  } else if (value.length < 4) {
+    error = 'A senha deve ter mais que 4 caracteres';
+  }
+  return error;
+};
+
+const validateEmail = (value) => {
+  let error;
+  if (!value) {
+    error = 'Digite seu e-mail';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+    error = 'E-mail invalido';
+  }
+  return error;
+};
+
+const Login = ({ history, loading, error, loginUserAction }) => {
+  const [email] = useState('demo@gogo.com');
+  const [password] = useState('gogo123');
+
+  useEffect(() => {
+    if (error) {
+      NotificationManager.warning(error, 'Login Error', 3000, null, null, '');
+    }
+  }, [error]);
+
+  const onUserLogin = (values) => {
+    if (!loading) {
+      if (values.email !== '' && values.password !== '') {
+        loginUserAction(values, history);
+      }
+    }
+  };
+
+  const initialValues = { email, password };
+
+  return (
+    <Row className="h-100">
+      <Colxx xxs="12" md="10" className="mx-auto my-auto">
+        <Card className="auth-card">
+          <div className="position-relative image-side ">
+            <div>.</div>
+          </div>
+          <div className="form-side">
+            <NavLink to="/" className="white">
+              <span className="logo-single" />
+            </NavLink>
+            <CardTitle className="mb-4">Acesso ao painel de doações </CardTitle>
+
+            <Formik initialValues={initialValues} onSubmit={onUserLogin}>
+              {({ errors, touched }) => (
+                <Form className="av-tooltip tooltip-label-bottom">
+                  <FormGroup className="form-group has-float-label">
+                    <Label>
+                      <IntlMessages id="user.email" />
+                    </Label>
+                    <Field
+                      className="form-control"
+                      name="email"
+                      validate={validateEmail}
+                    />
+                    {errors.email && touched.email && (
+                      <div className="invalid-feedback d-block">
+                        {errors.email}
+                      </div>
+                    )}
+                  </FormGroup>
+                  <FormGroup className="form-group has-float-label">
+                    <Label>Senha </Label>
+                    <Field
+                      className="form-control"
+                      type="password"
+                      name="password"
+                      validate={validatePassword}
+                    />
+                    {errors.password && touched.password && (
+                      <div className="invalid-feedback d-block">
+                        {errors.password}
+                      </div>
+                    )}
+                  </FormGroup>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <NavLink to="/user/forgot-password">
+                      Esuqueceu sua senha?{' '}
+                    </NavLink>
+                    <Button
+                      color="primary"
+                      className={`btn-shadow btn-multiple-state ${
+                        loading ? 'show-spinner' : ''
+                      }`}
+                      size="lg"
+                    >
+                      <span className="spinner d-inline-block">
+                        <span className="bounce1" />
+                        <span className="bounce2" />
+                        <span className="bounce3" />
+                      </span>
+                      <span className="label">Entrar </span>
+                    </Button>
+                  </div>
+                  <Row className="h-100 mt-3">
+                    <Colxx xxs="12" md="10">
+                      <NavLink to="/user/register" className="dark d-block">
+                        Não tem conta? Clique aqui para criar!
+                      </NavLink>
+                    </Colxx>
+                  </Row>
+                </Form>
+              )}
+            </Formik>
+          </div>
+        </Card>
+      </Colxx>
+    </Row>
+  );
+};
+const mapStateToProps = ({ authUser }) => {
+  const { loading, error } = authUser;
+  return { loading, error };
+};
+
+export default connect(mapStateToProps, {
+  loginUserAction: loginUser,
+})(Login);
